da02b41a183d48639afebc47af927cb2e395e6a2
Hasnae387 <hasnaebouchareb387@gmail.com>
2024-01-12 15:47:30 +0100

    La fonction d'ajout d'un produit

---
+++ src/main/java/com/ehei/ProduitService.java
@@ -0,0 +1,35 @@
+package com.ehei;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProduitService {
+
+	private List<Produit> produitsList = new ArrayList<>();
+	public boolean UnicitéProduit(Long id,String nom) {
+		   for (Produit p : produitsList) {
+		        if (p.getId().equals(id) || p.getNom().equals(nom)) {
+		            return true;
+		        }
+		    }
+		    return false;
+	}
+	
+	public boolean DonneesValides(Produit p) {
+	    return p.getPrix() >= 0 && p.getQuantite() >= 0;
+	}
+	
+	
+	public void ajouterProduit(Produit p) {
+		if (UnicitéProduit(p.getId(),p.getNom())) {
+			  throw new IllegalArgumentException("il existe déja un produit qui a le même id ou le nom.");
+		}
+		if (!DonneesValides(p)) {
+			  throw new IllegalArgumentException("Le prix et la quantité doivent être toujours positifs");
+		}
+		produitsList.add(p);
+	}
+		
+	
+	
+}
